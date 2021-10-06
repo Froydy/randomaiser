0d4d5cda1d66f086db19cd769cd2fa016360216c
Froydy <58701557+Froydy@users.noreply.github.com>
2021-10-07 01:47:53 +0300

    test
    test

---
+++ src/index.js
@@ -0,0 +1,34 @@
+function getRandomNumber(){
+  return Math.ceil(Math.random() * 10);
+}
+   
+function isNumbersCoincide(){ 
+  var randomNumber = getRandomNumber();
+  console.log(randomNumber);  
+  do{
+    user = prompt("Try to guess a number from 1 to 10:");
+    if(user === null){
+      console.clear();
+      console.log("You canceled the game.")
+      return;
+      }
+
+    if(Number(user) === randomNumber){
+      console.clear();
+      console.log("Correct!!!");
+      return;
+    }
+
+    console.clear()
+    console.log("Wrong. Try again!")
+  }
+  while(user !== randomNumber)
+}
+
+
+isNumbersCoincide();
+
+
+
+
+
